dab406e07c08df9be113475f75996f3e36d44ea1
Ian Kabaka <iankabaka1@gmail.com>
2024-09-25 12:08:08 +0300

    returning inner functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(niceActivity = "roller-skate") {
+  return `This Saturday, I want to ${niceActivity}!`;
+}
+const mondayWork = function (walk = "go to the office") {
+  return `This Monday, I will ${walk}.`;
+};
+function wrapAdjective(wrapperAdj = "*") {
+  return function (anyVal = "special") {
+    return `You are ${wrapperAdj}${anyVal}${wrapperAdj}!`;
+  };
+}
